9c0680cc26b752d6d705f82a7258ca8126601d85
ChiragKataria22 <chiragkataria11@gmail.com>
2021-07-31 12:48:33 +0530

    css support added for prod

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,8 +1,6 @@
 const scriptExtensions = /\.(tsx|ts|js|jsx|mjs)$/;
-const styleExtensions = /\.(css|less|styl|scss|sass|sss)$/;
 const imageExtensions = /\.(bmp|gif|jpg|jpeg|png)$/;
 const fontsExtension = /\.(eot|otf|ttf|woff|woff2)$/;
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   resolve: {
@@ -15,18 +13,6 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
-      // {
-      //   // Preprocess our own style files
-      //   test: styleExtensions,
-      //   exclude: /node_modules/,
-      //   use: ["style-loader", "css-loader"],
-      // },
-      // {
-      //   // Preprocess 3rd party style files located in node_modules
-      //   test: styleExtensions,
-      //   include: /node_modules/,
-      //   use: ["style-loader", "css-loader"],
-      // },
       {
         test: fontsExtension,
         type: 'asset',
